366a4f223e0f05ae65fe0362f585fa5b6feaf1f9
Jesse McDonald <nybble41@gmail.com>
2021-08-27 12:48:59 -0500

    rename Message to Callback

--- src/main.rs
+++ src/main.rs
@@ -13,16 +13,16 @@ struct Counter {
 }
 
 #[derive(Clone, Copy)]
-struct Message(fn(&mut Counter));
+struct Callback(fn(&mut Counter));
 
-impl Debug for Message {
+impl Debug for Callback {
     fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), Error> {
-        f.debug_struct("Message").finish_non_exhaustive()
+        f.debug_struct("Callback").finish_non_exhaustive()
     }
 }
 
 impl Sandbox for Counter {
-    type Message = Message;
+    type Message = Callback;
 
     fn new() -> Self {
         Self::default()
@@ -32,23 +32,23 @@ impl Sandbox for Counter {
         String::from("Counter - Iced")
     }
 
-    fn update(&mut self, message: Message) {
-        let Message(f) = message;
+    fn update(&mut self, message: Self::Message) {
+        let Callback(f) = message;
         f(self)
     }
 
-    fn view(&mut self) -> Element<Message> {
+    fn view(&mut self) -> Element<Self::Message> {
         Column::new()
             .padding(20)
             .align_items(Align::Center)
             .push(
                 Button::new(&mut self.increment_button, Text::new("Increment"))
-                    .on_press(Message(|c| c.value += 1))
+                    .on_press(Callback(|c| c.value += 1))
             )
             .push(Text::new(self.value.to_string()).size(50))
             .push(
                 Button::new(&mut self.decrement_button, Text::new("Decrement"))
-                    .on_press(Message(|c| c.value -= 1))
+                    .on_press(Callback(|c| c.value -= 1))
             )
             .into()
     }
